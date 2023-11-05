197fe27d1cfa44cdff50c0fb4387ee2d54c7189d
terisuke <terisuke1115@gmail.com>
2023-11-06 07:37:59 +0900

    hidden pass

--- scraper.py
+++ scraper.py
@@ -1,25 +1,29 @@
 import logging
 import gspread
+from gspread.exceptions import *
+import time
 from oauth2client.service_account import ServiceAccountCredentials
 from selenium import webdriver
 from selenium.common.exceptions import (NoSuchElementException, TimeoutException, WebDriverException)
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By  # Import By from selenium.webdriver.common
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Configure logging at the start
 logging.basicConfig(level=logging.INFO)
+
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/teradakousuke/Library/Mobile Documents/com~apple~CloudDocs/Scriping tool/credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 gc = gspread.authorize(creds)
 
 # Set Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 
 # Install ChromeDriver
 chrome_service = Service(ChromeDriverManager().install())
@@ -45,20 +49,34 @@ for i in range(1, 8):  # As li:nth-child goes up to 7
         else:
             break  # Stop the inner loop if no element is found for the current index
 
+def log_and_sleep_on_rate_limit(e):
+    logging.error(f"Rate limit error occurred: {e}")
+    time.sleep(60)  # Sleep for 60 seconds if rate limited
+
 # Use the previously authenticated client
 try:
     wb = gc.open("AllCentersData")
 except gspread.SpreadsheetNotFound:
-    wb = gc.create("AllCentersData")
+    try:
+        wb = gc.create("AllCentersData")
+    except Exception as e:
+        logging.error(f"Failed to create a spreadsheet: {e}")
+        exit(1)  # Exit if cannot create a spreadsheet
+except Exception as e:
+    logging.error(f"Failed to open the spreadsheet: {e}")
+    exit(1)  # Exit if cannot open a spreadsheet
 
-worksheet = wb.get_worksheet(0)
+try:
+    worksheet = wb.get_worksheet(0)
+except Exception as e:
+    logging.error(f"Failed to get the first worksheet: {e}")
+    exit(1)  # Exit if cannot get the worksheet
 
 # Add a header row if needed
 if worksheet.row_count == 0:
     worksheet.append_row(['事業所名', 'アクセス', '住所', '電話番号'])
 
 all_rows = []  # List to hold all rows of data
-
 
 # Extract data from each region's page
 for region_link in region_links:
@@ -97,6 +115,8 @@ for region_link in region_links:
                 # Append the data to the all_rows list
                 all_rows.append([element1_name, element2, element3, element4_tel])
 
+                # Delay between requests
+                time.sleep(1)  # Sleep for 1 second between each request
             except NoSuchElementException as e:
                 logging.error(f"Failed to find an element for center ID {center_id}: {e}")
             except TimeoutException as e:
@@ -105,10 +125,21 @@ for region_link in region_links:
             
     except WebDriverException as e:
         logging.error(f"Web driver error occurred while processing region link {region_link}: {e}")
+        # If rate limited, wait longer
+        if "429" in str(e):
+            time.sleep(60)  # Sleep for 60 seconds if rate limited
 
 # Append all rows to the worksheet in one batch
 if all_rows:
-    worksheet.append_rows(all_rows)
+    try:
+        worksheet.append_rows(all_rows)
+    except gspread.exceptions.APIError as e:
+        if e.response.status_code == 429:
+            log_and_sleep_on_rate_limit(e)
+        else:
+            logging.error(f"Failed to append rows to the worksheet: {e}")
+    except Exception as e:
+        logging.error(f"An error occurred while appending rows: {e}")
 
 # Close WebDriver
 driver.quit()
